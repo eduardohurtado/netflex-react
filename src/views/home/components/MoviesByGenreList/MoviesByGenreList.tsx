f51aef5ec9706ddf2d4c75faaec464693b662b22
Eduardo Hurtado <leduardohurtado@estudiante.uniajc.edu.co>
2021-01-24 13:00:26 -0500

    ayudame ome :v

--- src/views/home/components/MoviesByGenreList/MoviesByGenreList.tsx
+++ src/views/home/components/MoviesByGenreList/MoviesByGenreList.tsx
@@ -20,19 +20,40 @@ const navigationIcon: React.CSSProperties = {
   cursor: "pointer"
 };
 
-import { fetchGenre, fetchMovieByGenre } from "../../../../services";
+// Services
+import {
+  fetchGenre,
+  fetchMovieByGenre,
+  fetchMovies
+} from "../../../../services";
 
 export default function MoviesByGenreList(): JSX.Element {
   const [genres, setGenres] = useState([]);
   const [movieByGenre, setMovieByGenre] = useState([]);
+  const [nowPlaying, setNowPlaying] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
       setGenres(((await fetchGenre()) as unknown) as []);
       setMovieByGenre(((await fetchMovieByGenre(28)) as unknown) as []);
+      setNowPlaying(((await fetchMovies()) as unknown) as []);
     };
 
     fetchAPI();
+
+    // Esto es lo que he intentado *****
+
+    // Toggle button "active" class on press
+    // const element = document.getElementById("genresButton");
+    // // or whatever triggers the toggle
+    // const trigger = document.getElementById("js-toggle-sidebar");
+
+    // if (element) {
+    //   element.addEventListener("click", (e) => {
+    //     const element = (e as unknown) as HTMLElement;
+    //     element.classList.add("active");
+    //   });
+    // }
   }, []);
 
   const handleGenreClick = async (genre_id: number) => {
@@ -44,6 +65,7 @@ export default function MoviesByGenreList(): JSX.Element {
       <li key={index} className="list-inline-item">
         <button
           className="btn btn-outline-info"
+          id="genresButton"
           type="button"
           onClick={() => {
             handleGenreClick(item.id);
@@ -55,7 +77,30 @@ export default function MoviesByGenreList(): JSX.Element {
     );
   });
 
-  const movieList = movieByGenre.slice(0, 8).map((item: IMovies, index) => {
+  const movieListByGenre = movieByGenre
+    .slice(0, 8)
+    .map((item: IMovies, index) => {
+      return (
+        <div className="col-md-3" key={index}>
+          <div className="card">
+            <Link to={`/MovieDetails/${item.id}`}>
+              <img className="img-fluid" src={item.poster} alt={item.title} />
+            </Link>
+          </div>
+          <div className="mt-3">
+            <p style={{ fontWeight: "bolder" }}>{item.title} </p>
+            <p>Rated: {item.rating} </p>
+            <ReactStars
+              count={item.rating}
+              size={20}
+              color1={"#f4c10f"}
+            ></ReactStars>
+          </div>
+        </div>
+      );
+    });
+
+  const movieList = nowPlaying.slice(0, 8).map((item: IMovies, index) => {
     return (
       <div className="col-md-3" key={index}>
         <div className="card">
@@ -94,6 +139,8 @@ export default function MoviesByGenreList(): JSX.Element {
           </div>
         </div>
       </div>
+
+      {/* <div className="row mt-3">{movieListByGenre}</div> */}
 
       <div className="row mt-3">{movieList}</div>
     </>
